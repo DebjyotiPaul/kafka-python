6c67668612d7029b9cb712d3116817fc43453810
PhotonatedQubit <11320427+DebjyotiPaul@users.noreply.github.com>
2024-09-22 18:28:00 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,15 @@ def main():
     # Uncomment this to pass the first stage
     ##
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    #server.accept() # wait for client
+    client_socket, _ = server.accept()  # wait for client
+    client_socket.recv(1024)
+    msg_len = 0
+    correlation_id = 7
+    client_socket.sendall(msg_len.to_bytes(4, byteorder="big", signed=True))
+    client_socket.sendall(correlation_id.to_bytes(4, byteorder="big", signed=True))
+    client_socket.close()
+    server.close()
 
 
 if __name__ == "__main__":
