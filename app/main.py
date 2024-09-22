8bd2281c0916979732d7194b0490a24e127033e0
PhotonatedQubit <11320427+DebjyotiPaul@users.noreply.github.com>
2024-09-22 18:47:22 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket  # noqa: F401
 
+def create_message(id):
+    id_bytes = id.to_bytes(4, byteorder="big")
+    return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
+
+def handle_client(client):
+    req = client.recv(1024)
+    coRelationId = int.from_bytes(req[8:12], byteorder="big")
+    client.sendall(create_message(coRelationId))
+    client.close()
 
 def main():
     # You can use print statements as follows for debugging,
@@ -10,14 +19,9 @@ def main():
     ##
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     #server.accept() # wait for client
-    client_socket, _ = server.accept()  # wait for client
-    client_socket.recv(1024)
-    msg_len = 0
-    correlation_id = 7
-    client_socket.sendall(msg_len.to_bytes(4, byteorder="big", signed=True))
-    client_socket.sendall(correlation_id.to_bytes(4, byteorder="big", signed=True))
-    client_socket.close()
-    server.close()
+    while True:
+        client, addr = server.accept()
+        handle_client(client)
 
 
 if __name__ == "__main__":
